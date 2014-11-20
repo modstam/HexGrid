e0f1584a8cedf70f75840630e2d54f9b49d912c4
modstam <modstam@live.com>
2014-11-20 19:49:17 +0100

    Added project files

---
+++ Assets/Scripts/Grid/LevelCreator.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelCreator : MonoBehaviour {
+
+	public GameObject hexWirePrefab;
+	public Node[,] gridList;
+
+	public int ySize;
+	public int xSize;
+
+
+	// Use this for initialization
+	void Start () {
+		if(!hexWirePrefab || (xSize == null || ySize == null)){
+			Debug.Log ("LevelCreator members not set properly");
+		}
+		else{
+			gridList = new Node[xSize, ySize];
+			Vector3 position = new Vector3(0,0,0);
+			Quaternion rotation = Quaternion.Euler (new Vector3(-90,-180,0));
+
+			GameObject tmp = (GameObject)Instantiate(hexWirePrefab, position, rotation);
+			Node node = tmp.GetComponent<Node>();
+			gridList[0,0] = node;
+			Vector3 boundsSize = node.renderer.bounds.size;
+			position.x += boundsSize.x;
+			bool even = true;
+
+			for(int i = 0; i < xSize; i++){
+
+				if((i%2)!=0){ //uneven
+					even=false;
+					position.x = 0f;
+					position.x += boundsSize.x/2;
+					position.z -= (boundsSize.z - (boundsSize.z/4));
+				}
+				else{ //even
+					if(i!=0){
+					even=true;
+					position.x = 0f;
+					position.z -= (boundsSize.z - (boundsSize.z/4));
+					}
+				}
+
+				for(int j = 0; j < ySize; j++){
+					if((i==0 && j==0) || (j==(ySize-1) && !even)){
+						continue; //skip first iteration because of previous initialization
+					}
+					tmp = (GameObject)Instantiate(hexWirePrefab, position, rotation);
+					node = tmp.GetComponent<Node>();
+					gridList[i,j] = node;
+					position.x += boundsSize.x; 
+				}
+			}
+		}	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/Grid/Node.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Node : MonoBehaviour {
+	
+	public Node dir_1;
+	public Node dir_2;
+	public Node dir_3;
+	public Node dir_4;
+	public Node dir_5;
+	public Node dir_6;
+
+	public float cost;
+
+	void Start(){
+		initializeHex();
+	}
+
+	public void initializeHex(Node d1, Node d2, Node d3, Node d4 , Node d5 ,Node d6 ){
+		dir_1 = d1;
+		dir_2 = d2;
+		dir_3 = d3;
+		dir_4 = d4;
+		dir_5 = d5;
+		dir_6 = d6;
+	}
+	
+	public void initializeHex(){
+		//Placeholder atm
+		//Debug.Log ("Hex: " + getFullPosition() + " initialized");
+	}
+
+	public Vector3 getFullPosition(){
+		return this.transform.position;
+	}
+
+	public float getPositionX(){
+		return this.transform.position.x;
+	}
+
+	public float getPositionY(){
+		return this.transform.position.y;
+	}
+
+	public float getPositionZ(){
+		return this.transform.position.z;
+	}
+	
+
+
+
+}
